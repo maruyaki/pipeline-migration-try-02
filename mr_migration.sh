e5ebaa6fe12d5c5357550bb9bc5591a25a94b2a4
maruyaki <mtwo.m@mail.anemos-craft.com>
2021-08-01 23:14:11 +0000

    Update mr_migration.sh

--- mr_migration.sh
+++ mr_migration.sh
@@ -3,10 +3,10 @@
 export RESPONSE_CHK="False"
 while [ "`echo ${RESPONSE_CHK}`" = "False" ]
 do
-  export RESPONSE=$(curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${USER_TOKEN}" "https://api.github.com/repos/maruyaki/pipeline-migration-try-02/pulls" -d "{\"head\": \"${CI_MERGE_REQUEST_SOURCE_BRANCH_NAME}\", \"base\": \"${CI_MERGE_REQUEST_TARGET_BRANCH_NAME}\", \"title\": \"${CI_MERGE_REQUEST_TITLE}\"}")
-  echo ${RESPONSE} > log.txt
-  export RESPONSE_FILTERED=`echo ${RESPONCE} | grep "Fail"`
-  echo ${RESPONSE_FILTERED}
+  export RESPONSE=$(curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $1" "https://api.github.com/repos/maruyaki/pipeline-migration-try-02/pulls" -d "{\"head\": \"$2\", \"base\": \"$3\", \"title\": \"$4\"}")
+  echo $RESPONSE > log.txt
+  export RESPONSE_FILTERED=`echo $RESPONCE | grep "Fail"`
+  echo $RESPONSE_FILTERED
   
-  if [ "${RESPONSE_FILTERED}" = "" ]; then export RESPONSE_CHK="True"; fi
+  if [ "$RESPONSE_FILTERED" = "" ]; then export RESPONSE_CHK="True"; fi
 done
